a6616813cf84026b7843040add02529cadbad45b
Derek Wan <d.wan@icloud.com>
2023-07-04 09:06:50 +0800

    Finish part 1 (setup) (#3)

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>This is the second line</Text>
+    </View>
+  );
+}
