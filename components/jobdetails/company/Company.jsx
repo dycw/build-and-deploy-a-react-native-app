57ce8f3a65baffaffb8b5ff2f3cd0205bcbb206a
Derek Wan <d.wan@icloud.com>
2023-07-13 14:41:30 +0900

    Finish lesson 4 (job details screen) (#6)

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
+import styles from "./company.style";
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <View style={styles.jobTitle}>{jobTitle}</View>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} /</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   );
 };
